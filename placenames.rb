d030734474804818ac53da76e84215911b7ac0ed
Max Woolf <max@woolf.io>
2017-01-14 21:28:27 +0000

    adjusted for 1in deep wooden block

--- placenames.rb
+++ placenames.rb
@@ -6,7 +6,7 @@ Pathname.new('output').children.each { |p| p.unlink }
 names = YAML.load_file('data/guests.yml')
 
 FONT_SIZE = 60
-CARD_HEIGHT = 6.96.cm
+CARD_HEIGHT = 8.96.cm
 CARD_WIDTH = 10.24.cm
 
 names.each_slice(4) do |batch|
@@ -18,7 +18,7 @@ names.each_slice(4) do |batch|
     pdf.font('MoonFlower')
     pdf.stroke_color "f6f6f6"
     # Top left
-    name_y = 550
+    name_y = (700 - CARD_HEIGHT) + 1.4.in
     name_x = 255 + (-20 * batch_names[0].length - 1)
     pdf.draw_text batch_names[0], at: [name_x, name_y], size: FONT_SIZE
     pdf.stroke do
@@ -26,7 +26,7 @@ names.each_slice(4) do |batch|
     end
 
     # Top right
-    name_y = 550
+    name_y = (700 - CARD_HEIGHT) + 1.4.in
     name_x = 550 + (-20 * batch_names[1].length - 1)
     pdf.draw_text batch_names[1], at: [name_x, name_y], size: FONT_SIZE
     pdf.stroke do
@@ -34,7 +34,7 @@ names.each_slice(4) do |batch|
     end
 
     # Bottom left
-    name_y = 250
+    name_y = (400 - CARD_HEIGHT) + 1.4.in
     name_x = 255 + (-20 * batch_names[2].length - 1)
     pdf.draw_text batch_names[2], at: [name_x, name_y], size: FONT_SIZE
     pdf.stroke do
@@ -42,7 +42,7 @@ names.each_slice(4) do |batch|
     end
 
     # Bottom right
-    name_y = 250
+    name_y = (400 - CARD_HEIGHT) + 1.4.in
     name_x = 550 + (-20 * batch_names[3].length - 1)
     pdf.draw_text batch_names[3], at: [name_x, name_y], size: FONT_SIZE
     pdf.stroke do
