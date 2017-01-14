7220bea37a4295879ce1b9eae7e0a0ac445ce976
Max Woolf <max@woolf.io>
2017-01-14 21:10:22 +0000

    update

--- placenames.rb
+++ placenames.rb
@@ -5,48 +5,48 @@ require 'prawn/measurement_extensions'
 Pathname.new('output').children.each { |p| p.unlink }
 names = YAML.load_file('data/guests.yml')
 
+FONT_SIZE = 60
+CARD_HEIGHT = 6.96.cm
+CARD_WIDTH = 10.24.cm
+
 names.each_slice(4) do |batch|
-
-  Prawn::Document.generate("output/#{batch.join('-')}.pdf", page_size: 'A4', page_layout: :portrait, margin: [0,0,0,0]) do |pdf|
-
-    # Top left
+  batch_names = batch.map do |name|
+    name.split(' ')[0]
+  end
+  Prawn::Document.generate("output/#{batch_names.join('-')}.pdf", page_size: 'A4', page_layout: :portrait, margin: [0,0,0,0]) do |pdf|
     pdf.font_families.update("MoonFlower" => { normal: 'fonts/Moon Flower.ttf' })
-    pdf.stroke_axis
     pdf.font('MoonFlower')
-    pdf.draw_text batch[0], at: [200, 475], size: 60
+    pdf.stroke_color "f6f6f6"
+    # Top left
+    name_y = 550
+    name_x = 255 + (-20 * batch_names[0].length - 1)
+    pdf.draw_text batch_names[0], at: [name_x, name_y], size: FONT_SIZE
     pdf.stroke do
-      pdf.horizontal_line 2, (2 + 10.24.cm), at: 700 - (8.96.cm / 2)
-      pdf.rectangle [2, 700], 10.24.cm, 8.96.cm
+      pdf.rectangle [2, 700], CARD_WIDTH, CARD_HEIGHT
     end
 
     # Top right
-    pdf.font_families.update("MoonFlower" => { normal: 'fonts/Moon Flower.ttf' })
-    pdf.stroke_axis
-    pdf.font('MoonFlower')
-    pdf.draw_text batch[1], at: [475, 475], size: 60
+    name_y = 550
+    name_x = 550 + (-20 * batch_names[1].length - 1)
+    pdf.draw_text batch_names[1], at: [name_x, name_y], size: FONT_SIZE
     pdf.stroke do
-      pdf.horizontal_line 300, (300 + 10.24.cm), at: 700 - (8.96.cm / 2)
-      pdf.rectangle [300, 700], 10.24.cm, 8.96.cm
+      pdf.rectangle [300, 700], CARD_WIDTH, CARD_HEIGHT
     end
 
     # Bottom left
-    pdf.font_families.update("MoonFlower" => { normal: 'fonts/Moon Flower.ttf' })
-    pdf.stroke_axis
-    pdf.font('MoonFlower')
-    pdf.draw_text batch[2], at: [200, 170], size: 60
+    name_y = 250
+    name_x = 255 + (-20 * batch_names[2].length - 1)
+    pdf.draw_text batch_names[2], at: [name_x, name_y], size: FONT_SIZE
     pdf.stroke do
-      pdf.horizontal_line 2, (2 + 10.24.cm), at: 400 - (8.96.cm / 2)
-      pdf.rectangle [2, 400], 10.24.cm, 8.96.cm
+      pdf.rectangle [2, 400], CARD_WIDTH, CARD_HEIGHT
     end
 
     # Bottom right
-    pdf.font_families.update("MoonFlower" => { normal: 'fonts/Moon Flower.ttf' })
-    pdf.stroke_axis
-    pdf.font('MoonFlower')
-    pdf.draw_text batch[3], at: [475, 170], size: 60
+    name_y = 250
+    name_x = 550 + (-20 * batch_names[3].length - 1)
+    pdf.draw_text batch_names[3], at: [name_x, name_y], size: FONT_SIZE
     pdf.stroke do
-      pdf.horizontal_line 300, (300 + 10.24.cm), at: 400 - (8.96.cm / 2)
-      pdf.rectangle [300, 400], 10.24.cm, 8.96.cm
+      pdf.rectangle [300, 400], CARD_WIDTH, CARD_HEIGHT
     end
 
   end
